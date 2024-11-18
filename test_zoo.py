4660e1dcedc9349f59f0b79ba0ee0fc55c2fe716
KhetsoponLiu <88611712+KhetsoponLiu@users.noreply.github.com>
2024-11-18 17:25:37 +0000

    Added the zoo test cases

--- test_zoo.py
+++ test_zoo.py
@@ -10,5 +10,20 @@ class TestZoo(unittest.TestCase):
        
     # Add your additional test cases here.
 
+    def test_error_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Error")
+    
+    def test_gen_alpha_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(11), 50)
+     
+    def test_gen_z_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(18), 100)
+    
+    def test_adults_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(35), 150)
+    
+    def test_baby_boomer_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
 if __name__ == '__main__':
     unittest.main()
